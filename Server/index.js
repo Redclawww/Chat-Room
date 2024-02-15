1780055b596b106f8434f849241c561f5c7793e0
Raghav Sharma <redclaww02@gmail.com>
2024-02-15 23:43:32 +0530

    Deployment

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,8 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 import { Server } from "socket.io";
 import http from "http";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 const port = process.env.PORT || 1000;
 
@@ -11,43 +11,35 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(server,{
-    cors:{
-        origin: "https://chatroomclient.vercel.app",
-        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    }
+const io = new Server(server, {
+  cors: {
+    origin: "https://chatroomclient.vercel.app",
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+  },
 });
 
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User ID: ${socket.id} joined room: ${data}`);
+  });
 
+  socket.on("disconnect", () => {
+    console.log("User Disconnected...", socket.id);
+  });
 
-io.on("connection",(socket)=>{
-    console.log("A user connected");
-    socket.on("join_room",(data)=>{
-       socket.join(data);
-       console.log(`User ID: ${socket.id} joined room: ${data}`);
-        
-    });
-
-    socket.on("disconnect",()=>{
-        console.log("User Disconnected...",socket.id);
-    });  
-    
-    socket.on("chat message",(data)=>{
-        //for the sake of simplicity we’ll send the message to everyone, including the sender.
-        socket.broadcast.to(data.room).emit("recieve message", data);
-        console.log("Send Message Data:",data);
-    })
+  socket.on("chat message", (data) => {
+    //for the sake of simplicity we’ll send the message to everyone, including the sender.
+    socket.broadcast.to(data.room).emit("recieve message", data);
+    console.log("Send Message Data:", data);
+  });
 });
 
-
-
-app.get("/",(req,res)=>{
-    res.json("The Backend is working")
-})
-
-
-
-server.listen(port,()=>{
-    console.log("Server is running on port on 1000");
+app.get("/", (req, res) => {
+  res.json("The Backend is working");
 });
 
+server.listen(port, () => {
+  console.log("Server is running on port on 1000");
+});
